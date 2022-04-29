073fd69b446d0c5521018dfd91f90c5b10b6b5ff
Daniel McCarthy <42686497+dmcshiki@users.noreply.github.com>
2022-04-29 00:30:42 -0300

    Update futeep.py

--- futeep.py
+++ futeep.py
@@ -6,10 +6,10 @@ equipe = {
   "Guardiões FC": {"cidade": "Guardião", "torcedores": 40, "classico": True},
   "CA Protetores": {"cidade": "Guardião", "torcedores": 20, "classico": False},
   "SE Leões": {"cidade": "Leão", "torcedores": 40, "classico": True},
-  "Simba FC": {"cidade": "Leão", "torcedores": 15, "classico": False},
-  "SE Granada": {"cidade": "Granada", "torcedores": 10, "classico": False},
-  #"CA Lagos": {"cidade": "Lagos", "torcedores": 20, "classico": False},
-  #"Solaris RC": {"cidade": "Ponte-do-Sol", "torcedores": 30, "classico": False},
+  #"Simba FC": {"cidade": "Leão", "torcedores": 15, "classico": False},
+  #"SE Granada": {"cidade": "Granada", "torcedores": 10, "classico": False},
+  "CA Lagos": {"cidade": "Lagos", "torcedores": 20, "classico": False},
+  "Solaris RC": {"cidade": "Ponte-do-Sol", "torcedores": 30, "classico": False},
   #"Porto EC": {"cidade": "Porto", "torcedores": 45, "classico": True},
   #"Ferroviária EC": {"cidade": "Campos", "torcedores": 38, "classico": True},
   #"Portuários AA": {"cidade": "Porto", "torcedores": 12, "classico": False},
@@ -23,8 +23,8 @@ JOGOS = int(len(equipe)/2)
 
 combinacao_de_todos_jogos = np.empty(RODADAS*JOGOS, dtype=tuple)
 cont_combinacao_classicos = 0
-
 aux = 0
+
 for e1 in equipe.keys():
   for e2 in equipe.keys():
     if e1 != e2:
@@ -79,6 +79,7 @@ class JogoClassico(Restricao):
     jogosPossiveis = atribuicao
     rodadas = {}
     rClassico = {}
+    jogosRealizados = {}
 
     for i in range(RODADAS): # rodadas
       rodadas["R" + str(i)] = set()
@@ -87,16 +88,15 @@ class JogoClassico(Restricao):
     for variavel in jogosPossiveis.keys():
       rodada = variavel[0:2]
       times = jogosPossiveis[variavel]
-			
       if times is not None:
         time1 = times[0]
         time2 = times[1]
-				
         if equipe[time1]["classico"] in rClassico[rodada] and equipe[time2]["classico"] in rClassico[rodada]:
           return False
-        elif equipe[time1]["classico"] and equipe[time2]["classico"]:
+        elif equipe[time1]["classico"] == "true" and equipe[time2]["classico"] == "true":
           rodadas[rodada].add(time1)
           rodadas[rodada].add(time2)
+          jogosRealizados[f'{time1}{time2}'] = True
           rClassico[rodada].add(equipe[time1]["classico"])
           rClassico[rodada].add(equipe[time2]["classico"])
     return True
@@ -104,8 +104,8 @@ class JogoClassico(Restricao):
 if __name__ == "__main__":
     variaveis = []
   
-    if cont_combinacao_classicos < RODADAS:
-      for i in range(RODADAS):
+    if cont_combinacao_classicos <= RODADAS:
+      for i in range(RODADAS):  
         for j in range(JOGOS):
           # Variável RiJj, tal que i é o número da rodada e j é o jogo da rodada
           variaveis.append("R" + str(i) + "J" + str(j))
